30415a816ca1ff680b99710eb2871faeaef95978
jonathan-fontaine <jonathan.fontaine@gmail.com>
2020-05-11 12:35:41 +0200

    FrameTimeCode Package

---
+++ 1700-scripts/packages/FrameTimecodeToolbar/configure.js
@@ -0,0 +1,27 @@
+function configure(packageFolder, packageName) {
+  if (about.isPaintMode())
+    return;
+
+
+
+  var customToolToolbar = new ScriptToolbarDef({
+        id: "com.jonathanfontaine.FrameTimecodeToolbar",
+        text: "Frame Timecode Toolbar",
+        customizable: true,
+        floatable : true,
+        
+      });
+
+    customToolToolbar.addButton({
+          text: "Frame timecode Toolbar button",
+          icon: "showTimecode.png",
+          checkable: false,
+          action: "frameToolBar in ./FrameToolBar.js",
+        });
+      ScriptManager.addToolbar(customToolToolbar);
+
+
+}
+
+
+exports.configure = configure;
